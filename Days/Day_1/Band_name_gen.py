0d054efd8e2f97cef921997eea9007c487294ec6
ashraf0484 <64628528+ashraf0484@users.noreply.github.com>
2021-04-30 17:14:54 +0400

    Update Band_name_gen.py
    This python code will generate a band name for you based on the inputs that you give.

--- Days/Day_1/Band_name_gen.py
+++ Days/Day_1/Band_name_gen.py
@@ -1,3 +1,4 @@
 print("Welcome to the Band Name Generator.")
 print("Your band name could be " ,input("What's the name of the city you grew up in?\n"),
       input("What's your pet's name?\n"), "\n")
+
