6663fa2164a1b62a34d5b33f14fba56b7a8b0a02
ashraf0484 <64628528+ashraf0484@users.noreply.github.com>
2021-05-05 00:47:05 +0400

    update

---
+++ Days/Day_15/Coffee_machine/main.py
@@ -0,0 +1,60 @@
+from data import *
+from os import system
+
+system("clear")
+
+
+def resources_updater(drink):
+    """Updates the level of resources_left once a drink is made"""
+    if drink != "espresso":
+        resources["milk"] -= MENU[drink]["ingredients"]["milk"]
+    resources["water"] -= MENU[drink]["ingredients"]["water"]
+    resources["coffee"] -= MENU[drink]["ingredients"]["coffee"]
+
+
+def report_printer(resources_left):
+    """Prints the report for the remaining resources"""
+    water = resources_left["water"]
+    milk = resources_left["milk"]
+    coffee = resources_left["coffee"]
+    print(f"Water: {water}ml")
+    print(f"Milk: {milk}ml")
+    print(f"Coffee: {coffee}g")
+    print(f"Money: ${money}")
+
+
+def resource_checker(drink):
+    """Checks and return boolean values if there's sufficient amount of resources available or not"""
+
+
+def coin_calculator(drink, total_money):
+    """Calculates the coins given by the customer."""
+    resources_updater(drink)
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickles = int(input("How many nickles?: "))
+    pennies = int(input("How many pennies?: "))
+    total = ((quarters * 25) + (dimes * 10) + (nickles * 5) + (pennies * 1)) / 100
+    rate = MENU[drink]["cost"]
+    if total < rate:
+        print("Insufficient coins")
+    else:
+        change = round(total - rate, 2)
+        print(f"Here is ${change} in change.")
+        total_money += rate
+        return total_money
+
+
+money = 0
+
+while resources["coffee"] > 5:
+    selection = input("What would you like? (espresso/latte/cappuccino)").lower()
+
+    if selection == "report":
+        report_printer(resources)
+    elif selection in MENU:
+        print("Please insert coins.")
+        money += coin_calculator(selection, money)
+        print(f"Here is your {selection} ☕️ Enjoy!")
+    else:
+        print("Enter a valid input.")
