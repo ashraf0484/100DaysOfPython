7f593433c5a7da98247917f38258f33c66778abf
ashraf0484 <64628528+ashraf0484@users.noreply.github.com>
2021-05-14 16:48:12 +0400

    update

--- Days/Day_16/OOPified_coffee_machine/main.py
+++ Days/Day_16/OOPified_coffee_machine/main.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
@@ -23,9 +23,3 @@ while is_on:
         if coffee_maker.is_resource_sufficient(drink):
             if money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
-
-
-
-
-
-
